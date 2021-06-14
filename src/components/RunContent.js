1c7b74fe24d72b507cd22c4a5beac5f251d9be29
ezzard <ezzard.lopez@gmail.com>
2021-06-13 22:53:42 -0500

    Add files via upload

--- src/components/RunContent.js
+++ src/components/RunContent.js
@@ -8,9 +8,18 @@ function RunContent() {
       <div className="container">
         <RunText
           runTitle="Technical Management"
-          firstLine="Expect nothing less than smooth seas. "
-          br={<br />}
-          runText="Technical expertise is vital in maintaining the high performance of any vessel.  At ASM Yachts our Technical Division is available 24/7 to provide operational support and experienced advice for our entire fleet.  They can ensure that each yacht has the latest technology on board, for crew and guests, and that it exceeds the standards set by both local and international authorities.  "
+          firstLine={<span>Expect nothing less than smooth seas. </span>}
+          runText={
+            <span>
+              Technical expertise is vital in maintaining the high performance
+              of any vessel. At ASM Yachts our Technical Division is
+              <strong>available 24/7</strong> to provide operational support and
+              experienced advice for our entire fleet. They can ensure that each
+              yacht has the latest <strong>technology</strong> on board, for
+              crew and guests, and that it exceeds the standards set by both
+              local and international authorities.
+            </span>
+          }
         />
         <hr />
         <RunText
